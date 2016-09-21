71951589187f18ffa02dade2310d44375be41ab3
David Millet <dmillet@box.com>
2016-09-20 17:50:43 -0700

    adding gamestream

--- client/GameClient.js
+++ client/GameClient.js
@@ -1,10 +1,14 @@
 
 import Game from '../game/Game.js';
+import { Socket } from 'gamestream';
 
 export default class GameClient {
 
 	constructor() {
 		this.game = new Game();
+		this.game.stream.on('data', (data) => { console.info(data); });
+		this.socket = new Socket();
+		this.socket.pipe(this.game.stream);
 	}
 
 }
